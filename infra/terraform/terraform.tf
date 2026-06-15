7493d5a34f089ebd44078a0270c9d776b4e7983e
yokozuna47 <ba.isskha@gmail.com>
2026-06-15 18:22:01 +0200

    terraform: rename provider.tf to terraform.tf, region from var

--- infra/terraform/terraform.tf
+++ infra/terraform/terraform.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.5.0"
 
   required_providers {
     aws = {
@@ -10,5 +10,5 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
