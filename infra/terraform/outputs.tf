9f79df15ce070356d6b33b4ac10feb050afd6806
Lanijil <jilaniless@gmail.com>
2026-06-16 15:54:03 +0200

    fin mise en place pour TD

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,19 +1,19 @@
 output "instance_public_ip" {
   description = "IP publique de l'instance EC2"
-  value       = aws_instance.yokozuna_serverweb.public_ip
+  value       = aws_instance.jilani_serverweb.public_ip
 }
 
 output "instance_id" {
   description = "ID de l'instance EC2"
-  value       = aws_instance.yokozuna_serverweb.id
+  value       = aws_instance.jilani_serverweb.id
 }
 
 output "subnet_id" {
   description = "ID du subnet"
-  value       = aws_subnet.yokozuna_subnet.id
+  value       = aws_subnet.jilani_subnet.id
 }
 
 output "security_group_id" {
   description = "ID du security group"
-  value       = aws_security_group.yokozuna_sg.id
+  value       = aws_security_group.jilani_sg.id
 }
