d0703a476b8bfd647b467de6352f9651c59c29a7
Benit Musindi <b.musindi@ecole-ipssi.net>
2026-06-16 16:41:49 +0200

    fichier output update

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,19 +1,21 @@
 output "instance_public_ip" {
   description = "IP publique de l'instance EC2"
-  value       = aws_instance.yokozuna_serverweb.public_ip
+  value       = aws_instance.musin_serverweb.public_ip
 }
 
 output "instance_id" {
   description = "ID de l'instance EC2"
-  value       = aws_instance.yokozuna_serverweb.id
+  value       = aws_instance.musin_serverweb.id
 }
 
 output "subnet_id" {
   description = "ID du subnet"
-  value       = aws_subnet.yokozuna_subnet.id
+  value       = aws_subnet.musin_subnet.id
 }
 
 output "security_group_id" {
   description = "ID du security group"
-  value       = aws_security_group.yokozuna_sg.id
+  value       = aws_security_group.musin_sg.id
 }
+
+
